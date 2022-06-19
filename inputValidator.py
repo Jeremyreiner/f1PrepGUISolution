ccf02c0b6183e89e2f7aba6cfefe78eb045f8d8b
jeremy reiner <reiner_jeremy@yahoo.com>
2022-06-19 18:17:30 +0300

    sdklj

--- inputValidator.py
+++ inputValidator.py
@@ -40,38 +40,40 @@ def ValidateInput(values) -> tuple:
         else:
             print(value)
             pass
+    ThrowPopUpError(inValidList)
+
     
-    if len(inValidList) > 0:
-        mapInputs = {
-            '-IMAGE-': 'Image Path',
-            '-EMBEDED_SRC-': 'Embedded_src_Path',
-            '-FARMSERVER-': 'farmserverfillsrc',
-            '-FARMSERVERF1INSTALLER-': 'farserverf1installersrc',
-            '-DB_FILE-': 'pathtodbfile',
-            '-NAND_SRC_PATH-': 'nand_src_path',
-            '-DB_FILE_SRC-': 'dbfilesrc',
-        }
+
+def ThrowPopUpError(inValidList):
+        if len(inValidList) > 0:
+            mapInputs = {
+                '-IMAGE-': 'Image Path',
+                '-EMBEDED_SRC-': 'Embedded_src_Path',
+                '-FARMSERVER-': 'farmserverfillsrc',
+                '-FARMSERVERF1INSTALLER-': 'farserverf1installersrc',
+                '-DB_FILE-': 'pathtodbfile',
+                '-NAND_SRC_PATH-': 'nand_src_path',
+                '-DB_FILE_SRC-': 'dbfilesrc',
+            }
         invalidMarkups = []
         for value in inValidList:
             invalidMarkups.append(mapInputs[value])
         
         sg.PopupError(f'Incorrect INPUT file/files for the following Catagories!\n {invalidMarkups}')
 
+
 def CheckValidFilePath(values, value):
     i_path = values[value]
     if i_path == '':
         pass
     if len(i_path) == 0:
-        #sg.PopupError('No INPUT file or folder selected!')
         return False, value
     elif not os.path.exists(i_path):
-        #sg.PopupError('INPUT file/folder does not exist!')
         return False, value
     elif os.path.isdir(i_path):
         ext_type = 'fs'
     else: # must be an existing file then
         if i_path.lower().endswith('.tar') and not i_path.lower().endswith('.zip'):
-            #sg.PopupError('Input file is not a supported archive! ', i_path)
             return False, value
         else:
             ext_type = Path(i_path).suffix[1:].lower() 
