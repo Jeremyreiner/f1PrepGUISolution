f97fc1cedf30c64579b31b922ca8ee11b7ef7480
Jeremy Reiner <reiner_jeremy@yahoo.com>
2022-12-21 12:56:39 +0200

    table updates all elements from json, and when clicked, updates the output window with the json object

--- f1ObjLogger/main.py
+++ f1ObjLogger/main.py
@@ -7,15 +7,25 @@ data = read_json(path)
 
 table_data = [[f'{x["id"]}', f'{x["name"]}'] for x in data]
 
-layout = [[sg.Table(values=table_data, headings=['ID', 'Name'], max_col_width=25, auto_size_columns=True, alternating_row_color='lightblue', key='table', enable_events=True)],
-          [sg.Multiline(size=(40, 5), key='output')]]
+layout = [
+    [
+        sg.Table(values=table_data, headings=['ID', 'Name'], 
+            max_col_width=25, auto_size_columns=True, 
+            alternating_row_color='lightblue', 
+            key='table', enable_events=True)
+    ],
+    [sg.Multiline(size=(40, 5), key='output')]
+]
 
 window = sg.Window('Table Example').Layout(layout)
 
 th = Thread(target=RunThread(data, len(data), path).Run)
+
 th.start()
+
 while True:
     event, values = window.Read(timeout=1000)
+    
     if event == 'table':
         if values['table'] != []:
             selected_row = values['table'][0]
@@ -24,10 +34,10 @@ while True:
     elif event == '__TIMEOUT__':
         data = read_json(path)
 
-        inter = len(data) - 1
+        table_data.clear()
+        window.FindElement('table').Update(values=table_data)
 
-        table_data.append([f'{data[inter]["id"]}', f'{data[inter]["name"]}'])
-
+        table_data = [[f'{x["id"]}', f'{x["name"]}'] for x in data]
         window.FindElement('table').Update(values=table_data)
         
     elif event is None:
